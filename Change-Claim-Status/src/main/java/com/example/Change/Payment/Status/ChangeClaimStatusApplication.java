ed3f04f203dddd522ae854bfa095c024fe4b1411
Maksim Lopin <lopin.maxim2013@yandex.ru>
2023-07-28 12:49:06 +0300

    Initial commit

---
+++ Change-Claim-Status/src/main/java/com/example/Change/Payment/Status/ChangeClaimStatusApplication.java
@@ -0,0 +1,13 @@
+package com.example.Change.Payment.Status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeClaimStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeClaimStatusApplication.class, args);
+	}
+
+}
---
+++ Change-Claim-Status/src/test/java/com/example/Change/Payment/Status/ChangePaymentStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Change.Payment.Status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangePaymentStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
